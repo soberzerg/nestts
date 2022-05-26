46054e7062dcffa41f47a2c4fb424e404518bd7e
Sergey Sobolev <sergej.a.sobolev@gmail.com>
2022-05-26 03:57:06 +0300

    disable cors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -5,6 +6,15 @@ import { PaginatedResponseDto } from './general/paginated-response.dto';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const disableCors = Boolean(configService.get('DISABLE_CORS', false));
+
+  if (disableCors) {
+    app.enableCors({
+      origin: false,
+    });
+  }
 
   app.enableShutdownHooks();
 
